0d81fcbf014c35179c50aaa4370d515bca6b4623
anitha ganesan <anithagk12@gmail.com>
2019-02-10 11:03:27 +0530

    Update import_csv_to_OSM.py

--- import_csv_to_OSM.py
+++ import_csv_to_OSM.py
@@ -14,7 +14,6 @@ with open('temple2.csv', newline='') as File:
     reader = csv.reader(File)
     for row in reader:
         print(row)
-        #print ("anitha")
         api.ChangesetCreate({u"comment": u"Siva Temple "+row[1].title()})
         data = { u"lat": row[3], u"lon": row[4], u"tag": { u"amenity": u"temple", u"name": row[1].title(),  u"location": row[2].title(), u"religion":u"hindu" } }
         api.NodeCreate(data)
